e692b34009bbe12601373ff823c15ac0638442db
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-03-16 13:04:19 +0200

    Додав нові прототипи

--- lab09/prj/Modules/ModulesIsachenkov.h
+++ lab09/prj/Modules/ModulesIsachenkov.h
@@ -3,7 +3,8 @@
 
 double s_calculation(int x, int y, int z);
 int Task_9_1(int posadOklad, int zarobPlata, int year);
-int Task_9_2(int one, int two, int three, int four, int five, int six);
+int Task_9_2_1(int one, int two, int three, int four, int five, int six);
+int Task_9_2_2(int celsia);
 int Task_9_3(int N);
 
 #endif // MODULESISACHENKOV_H_INCLUDED
